fb5ff49d0968506a4829274be5e8718c768a8ff4
TereBin <44892691+TereBin@users.noreply.github.com>
2022-08-19 16:08:42 +0900

    Create humanities.py

---
+++ 단과대/humanities.py
@@ -0,0 +1,35 @@
+import json
+import pprint as pp
+
+def humanities(student_json_path, student_dict, w):
+    i = 2
+    while len(w.row_values(i)) > 0:
+        data = w.row_values(i)
+        score = data[1]
+        email = data[-1]
+        print(email)
+        if score[0] == score[-1]:
+            print("정답")
+            if email in student_dict.keys():
+                print("기존 유저")
+                student_dict[email]["humanities"] = "True"
+                pp.pprint(student_dict)
+                with open(student_json_path, 'w', encoding='utf-8') as file:
+                    json.dump(student_dict, file, indent="\t")
+            else:
+                print("신규 유저")
+                student_dict[email] = {}
+                student_dict[email]["humanities"] = "True"
+                student_dict[email]["social"] = "False"
+                student_dict[email]["mas"] = "False"
+                student_dict[email]["natural"] = "False"
+                student_dict[email]["engineering"] = "False"
+                student_dict[email]["economic"] = "False"
+                student_dict[email]["business"] = "False"
+                pp.pprint(student_dict)
+                with open(student_json_path, 'w', encoding='utf-8') as file:
+                    json.dump(student_dict, file, indent="\t")
+
+        else:
+            print("오답\n")
+        i += 1
